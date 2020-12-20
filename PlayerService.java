9bdf5f5e777fb470c26e5020af1e6155a89ccc57
mahmoudeltohamy345 <mahmoudeltohamy345@gmail.com>
2020-12-20 21:28:21 +0200

    Updated addPlayerToSquad in UserService Added setPlayerID in Squad

--- PlayerService.java
+++ PlayerService.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PlayerService {
    private PlayerRepo playerRepo;
     void changePrice(double price,int playerId){
@@ -11,13 +13,23 @@ public class PlayerService {
     }
     void addNewPlayer(Player player)
     {
-
-        /*String playerName;
-        String playerPosition;
-         int playerValue;
-         int playerID;
-         String playerTeam;*/
-        player = new Player();
+        Scanner input = new Scanner(System.in);
+        Scanner intinput = new Scanner(System.in);
+        String s;
+        int value;
+        System.out.println("Please enter player name ");
+        s = input.nextLine();
+        player.setPlayerName(s);
+        System.out.println("Please enter player team");
+        s = input.nextLine();
+        player.setPlayerTeam(s);
+        System.out.println("Please enter player position");
+        s = input.nextLine();
+        player.setPlayerPosition(s);
+        System.out.println("Please enter player value");
+        value = intinput.nextInt();
+        player.setPlayerValue(value);
+        player.setPlayerID(playerRepo.playersMap.size());
         PlayerRepo.playersMap.put(player.getPlayerID(),player);
     }
 }
