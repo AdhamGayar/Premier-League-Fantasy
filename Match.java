c1b73cd9cd6c01d2b406eb87bc62e6a67becacd1
adnanwae <adnanwael2000@gmail.com>
2021-01-02 18:50:32 +0200

    Match class #1

--- Match.java
+++ Match.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Match {
+    private String homeTeam;
+    private String awayTeam;
+    private ArrayList<Integer> homePlayersId = new ArrayList<>(11);
+    private ArrayList<Integer> awayPlayersId = new ArrayList<>(11);
+    private String gameWeekMatch; // 0101
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
 }
